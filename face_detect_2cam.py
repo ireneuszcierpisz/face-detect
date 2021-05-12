e830207b553910caf60cc4c5a41c9e06c735233e
Irek <49306838+ireneuszcierpisz@users.noreply.github.com>
2021-05-13 00:42:09 +0100

    Update face_detect_2cam.py

--- face_detect_2cam.py
+++ face_detect_2cam.py
@@ -1,16 +1,5 @@
 """
-    Simple people detection application for testing packaging app openvino deployment tools.
-    We can use the Deployment Manager present in OpenVINO to create a runtime package from our application. 
-    To do this try the following:
-        Start the Deployment Manager in interactive mode
-        Select the hardware where you want to deploy your model
-        Select the folder containing your application code, models, and data
-    These packages can be easily sent to other hardware devices to be deployed.
-    To deploy the Inference Engine components from the development machine to the target host, perform the following steps:
-        Transfer the generated archive to the target host using your preferred method.
-        Unpack the archive into the destination directory on the target host (replace the openvino_deployment_package with the name you use).
-            For Linux:  tar xf openvino_deployment_package.tar.gz -C <destination_dir>
-            For Windows, use an archiver your prefer.
+    Simple face detect app using 2 or more cams.
 """
 
 from openvino.inference_engine import IENetwork, IECore
